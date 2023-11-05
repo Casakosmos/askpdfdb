1fc647a4832704677a9a906b5cdec894cc6bd0d5
casacosmos <132779702+casacosmos@users.noreply.github.com>
2023-11-05 15:59:40 -0400

    Update migration.sql

--- migration.sql
+++ migration.sql
@@ -7,6 +7,8 @@ CREATE SCHEMA IF NOT EXISTS my_schema;
 -- Set the search_path to include the new schema
 SET search_path TO my_schema, public;
 
+
+## Texts table will be embedded by openai
 -- Create the texts table
 CREATE TABLE IF NOT EXISTS texts (
     id bigserial PRIMARY KEY,
@@ -14,11 +16,13 @@ CREATE TABLE IF NOT EXISTS texts (
     embedding vector(1536)
 );
 
+## Questions and answers (answers will include responses) are embedded by cohere
+
 -- Create the questions table
 CREATE TABLE IF NOT EXISTS questions (
     id bigserial PRIMARY KEY,
     question text,
-    embedding vector(1536)
+    embedding vector(1024)
 );
 
 -- Create the answers table
@@ -26,15 +30,26 @@ CREATE TABLE IF NOT EXISTS answers (
     id bigserial PRIMARY KEY,
     question_id bigint REFERENCES questions(id),
     text_id bigint REFERENCES texts(id),
-    answer text
+    answer text,
+    embedding vector(1024)
+);
+
+-- Create the responses table
+CREATE TABLE IF NOT EXISTS responses (
+    id bigserial PRIMARY KEY,
+    response text,
+    embedding vector(1024)
 );
 
 -- Create the HNSW indexes
 CREATE INDEX IF NOT EXISTS texts_embedding_idx ON texts USING hnsw (embedding vector_l2_ops);
 CREATE INDEX IF NOT EXISTS questions_embedding_idx ON questions USING hnsw (embedding vector_l2_ops);
+CREATE INDEX IF NOT EXISTS answers_embedding_idx ON answers USING hnsw (embedding vector_l2_ops);
+CREATE INDEX IF NOT EXISTS responses_embedding_idx ON responses USING hnsw (embedding vector_l2_ops);
 
 -- Grant permissions to a specific user
 GRANT USAGE ON SCHEMA my_schema TO myuser;
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE texts TO myuser;
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE questions TO myuser;
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE answers TO myuser;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE responses TO myuser;
