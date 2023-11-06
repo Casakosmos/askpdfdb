93edf7c3bfa6aeb072b4086e9e09a56931aa3dfc
casacosmos <132779702+casacosmos@users.noreply.github.com>
2023-11-06 01:01:10 -0400

    Update migration.sql

--- migration.sql
+++ migration.sql
@@ -7,22 +7,33 @@ CREATE SCHEMA IF NOT EXISTS my_schema;
 -- Set the search_path to include the new schema
 SET search_path TO my_schema, public;
 
-
-## Texts table will be embedded by openai
--- Create the texts table
-CREATE TABLE IF NOT EXISTS texts (
-    id bigserial PRIMARY KEY,
-    text text,
-    embedding vector(1536)
+-- extracted_texts table will be embedded by Cohere
+-- Create the extracted_texts table
+CREATE TABLE philosophical_works (
+    id SERIAL PRIMARY KEY,
+    extracted_texts TEXT,
+    embedding VECTOR(1024),
+    author TEXT,
+    work TEXT,
+    date_written DATE
 );
 
-## Questions and answers (answers will include responses) are embedded by cohere
+
+-- Create the HNSW indexes
+CREATE INDEX IF NOT EXISTS extracted_texts_headline_embedding_idx ON extracted_texts USING gist (headline_embedding gist_l2_ops);
+
+-- Grant permissions to a specific user
+GRANT USAGE ON SCHEMA my_schema TO myuser;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE extracted_texts TO myuser;
+
+
+-- Questions and answers (answers will include responses) are embedded by OpenAI
 
 -- Create the questions table
 CREATE TABLE IF NOT EXISTS questions (
     id bigserial PRIMARY KEY,
     question text,
-    embedding vector(1536)
+    embedding vector(1536) -- OpenAI embeddings go here
 );
 
 -- Create the answers table
@@ -31,25 +42,18 @@ CREATE TABLE IF NOT EXISTS answers (
     question_id bigint REFERENCES questions(id),
     text_id bigint REFERENCES texts(id),
     answer text,
-    embedding vector(1536)
+    embedding vector(1536) -- OpenAI embeddings go here
 );
 
--- Create the responses table
-CREATE TABLE IF NOT EXISTS responses (
-    id bigserial PRIMARY KEY,
-    response text,
-    embedding vector(1536)
-);
 
 -- Create the HNSW indexes
-CREATE INDEX IF NOT EXISTS texts_embedding_idx ON texts USING hnsw (embedding vector_l2_ops);
+CREATE INDEX IF NOT EXISTS extracted_texts_embedding_idx ON texts USING hnsw (embedding vector_l2_ops);
 CREATE INDEX IF NOT EXISTS questions_embedding_idx ON questions USING hnsw (embedding vector_l2_ops);
 CREATE INDEX IF NOT EXISTS answers_embedding_idx ON answers USING hnsw (embedding vector_l2_ops);
-CREATE INDEX IF NOT EXISTS responses_embedding_idx ON responses USING hnsw (embedding vector_l2_ops);
 
 -- Grant permissions to a specific user
 GRANT USAGE ON SCHEMA my_schema TO myuser;
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE texts TO myuser;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE extracted_texts TO myuser;
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE questions TO myuser;
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE answers TO myuser;
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE responses TO myuser;
+
